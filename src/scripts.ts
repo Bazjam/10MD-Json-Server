e108762b998c37d709fed11380ad82ee9d4e19a0
Bazjam <bonusystem@gmail.com>
2023-01-22 23:33:14 +0200

    Add all btn data attribute

--- src/scripts.ts
+++ src/scripts.ts
@@ -5,8 +5,10 @@ type Animal = {
   name: string;
   description: string;
 };
-const wrapper = document.querySelector("[data-insert-cards]");
+
 const randomImg = "https://picsum.photos/id/237/200/300";
+
+const wrapper = document.querySelector("[data-insert-cards]");
 
 const btnAddNewObj = document.querySelector("[data-add-btn]");
 
@@ -21,7 +23,6 @@ document.addEventListener("DOMContentLoaded", function () {
 const loadAllData = () => {
   axios.get<Animal[]>("http://localhost:3000/animals").then(({ data }) => {
     data.forEach((element) => {
-      const addHtml = () => {
         const card = document.createElement("div"); //     <div class="card-wrapper">
         card.classList.add("card-wrapper");
 
@@ -32,12 +33,12 @@ const loadAllData = () => {
         <p class="">${element.name}</p>
         <p class="">${element.description}</p>
         
-        <button data-edit-btn>Edit</button>
+        <button data-edit-btn="${element.id}">Edit</button>
         <button>Delete</button>
         </div>
         <br>
         
-        <div data-wrapper-form class="editForm hide">
+        <div data-wrapper-form="${element.id}" class="editForm hide">
         <input type="text" id="text" rows="10" placeholder="New Title"></label>
         <br>
         <textarea id="text" rows="10" cols="15" placeholder="New description"></textarea>
@@ -46,27 +47,19 @@ const loadAllData = () => {
         </div>
         <br><br><br><br>`;
         wrapper.append(card);
-
-        const btnUpdate = document.querySelector("[data-update-btn]");
-        const btnEdit = document.querySelector("[data-edit-btn]");
-        const wrapperForm2 = document.querySelector("[data-wrapper-form]");
-
-        btnEdit.addEventListener("click", () => {
-          wrapperForm2.classList.remove("hide");
-        });
-
-        btnUpdate.addEventListener("click", () => {
-          wrapperForm2.classList.add("hide");
-        });
-      };
-      addHtml();
+      
     });
+    initEvents();
   });
+  
 };
+
 //---------------------------------------------------------------------------
 
 let userTitleText = "";
 let userDescriptionText = "";
+
+
 
 inputText.addEventListener("change", function () {
   userTitleText = this.value;
@@ -78,6 +71,29 @@ textareaText.addEventListener("change", function () {
   // userDescriptionText = this.value;
 });
 
+const initEvents = () => {
+
+  const btnEdit = document.querySelectorAll("[data-edit-btn]") as NodeListOf<HTMLElement>;   //All data attribute 
+  // const btnUpdate = document.querySelector("[data-update-btn]");
+  
+  
+  btnEdit.forEach((btn) => {                                      //All btn with data-edit-btn
+    btn.addEventListener("click", () => {                         // povesitj na vse knopki Event click
+      const cardID = btn.dataset.editBtn;                         // Dataset zabiraem value iz data attribute ([data-wrapper-form="2"])
+      const wrapperForm2 = document.querySelector(`[data-wrapper-form="${cardID}"]`);
+      wrapperForm2.classList.remove("hide");
+    });
+  });
+  
+  // btnEdit.addEventListener("click", () => {
+    //   wrapperForm2.classList.remove("hide");
+  // });
+
+  // btnUpdate.addEventListener("click", () => {
+  //   wrapperForm2.classList.add("hide");
+  // });
+};
+
 btnAddNewObj.addEventListener("click", () => {
   axios
     .post<Animal>("http://localhost:3000/animals", {
@@ -86,10 +102,9 @@ btnAddNewObj.addEventListener("click", () => {
     })
     .then(() => {
       wrapper.innerHTML = ""; //opustoshitj vse kartocki
-      loadAllData();          //zanovo zagruzitj dannie
-      inputText.value = '';
-      textareaText.value = '';
+      loadAllData(); //zanovo zagruzitj dannie
+      inputText.value = "";
+      textareaText.value = "";
     });
-
 });
 //---------------------------------------------------------------------------
